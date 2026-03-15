240281d1f242d7836109e63e3d541b7667fda384
Benjamin Chu <benchu99@hotmail.com>
2026-03-14 22:38:37 -0700

    make output location less verbose (more vlog like)

--- src/vlogify/geocode.py
+++ src/vlogify/geocode.py
@@ -8,10 +8,28 @@ def reverse_geocode(lat, lon):
     try:
         location = geolocator.reverse((lat, lon), language="en")
 
-        if location:
-            return location.address
+        if not location:
+            return "Unknown location"
+
+        addr = location.raw.get("address", {})
+
+        name = (
+            addr.get("tourism")
+            or addr.get("attraction")
+            or addr.get("leisure")
+            or addr.get("building")
+            or addr.get("amenity")
+        )
+
+        city = addr.get("city") or addr.get("town") or addr.get("village")
+
+        if name and city:
+            return f"{name}, {city}"
+
+        if city:
+            return city
+
+        return "Unknown location"
 
     except Exception:
-        pass
-
-    return "Unknown location"
+        return "Unknown location"
